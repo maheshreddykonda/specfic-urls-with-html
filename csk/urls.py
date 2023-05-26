216bcda522bd63b100a1f6d50f372f71a1a2e7de
maheshreddykonda <kondamaheshreddy1999@gmail.com>
2023-05-26 14:02:48 +0530

    first commit

---
+++ csk/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from csk.views import *
+app_name='anything'
+urlpatterns=[
+
+    path('msd/',msd,name='msd')
+]
---
+++ rcb/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from csk.views import *
+app_name='anything'
+urlpatterns=[
+
+    path('virat/',msd,name='virat')
+]
